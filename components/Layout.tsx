b25ec83fab97cdce2a4a52221af762b0674a68a0
Ikram Maulana <ikram075@ummi.ac.id>
2022-10-28 14:47:08 +0700

    Feat(UI): Add Layout

---
+++ components/Layout.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+
+export const siteTitle = "Dreambinar";
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+export default function Layout({children}: LayoutProps) {
+    return (
+        <div className="h-screen font-content antialiased">
+            <Head>
+                <link rel="icon" href="/favicon.ico"/>
+                <meta name="description" content="Dreambinar is a platform that can be used by everyone with the main features being able to search for many events and being able to create a webinar event and then share it with everyone with guaranteed security and interesting events."/>
+                <meta
+                    property="og:image"
+                    content={`https://og-image.vercel.app/${encodeURI(
+                        siteTitle
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+                <meta name="og:title" content={siteTitle}/>
+                <meta name="twitter:card" content="summary_large_image"/>
+            </Head>
+
+            <main>{children}</main>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import Layout, {siteTitle} from "../components/Layout";
+import Head from "next/head";
+
 export default function Home() {
     return (
-        <div>
+        <Layout>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
             <h1>Hello World</h1>
-        </div>
-    )
+        </Layout>
+    );
 }
